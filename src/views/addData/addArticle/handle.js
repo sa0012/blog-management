b4eda5f2273c4166d56ecbea21482fc92c92fbe2
chenlong <1583479514@qq.com>
2019-01-04 15:52:26 +0800

    编辑器

--- src/views/addData/addArticle/handle.js
+++ src/views/addData/addArticle/handle.js
@@ -26,7 +26,69 @@ export default {
   name: 'addArticle',
   data() {
     return {
-      articleContent: ''
+      articleContent: '',
+      toolArr: [
+        {
+          icon: '',
+          text: '撤销'
+        },
+        {
+          icon: '',
+          text: '重做'
+        },
+        {
+          icon: '',
+          text: '加粗'
+        },
+        {
+          icon: '',
+          text: '斜体'
+        },
+        {
+          icon: '',
+          text: '标题'
+        },
+        {
+          icon: '',
+          text: '删除线'
+        },
+        {
+          icon: '',
+          text: '无序列表'
+        },
+        {
+          icon: '',
+          text: '有序列表'
+        },
+        {
+          icon: '',
+          text: '待办事项'
+        },
+        {
+          icon: '',
+          text: '引用'
+        },
+        {
+          icon: '',
+          text: '代码'
+        },
+        {
+          icon: '',
+          text: '超链接'
+        },
+        {
+          icon: '',
+          text: '图片'
+        },
+        {
+          icon: '',
+          text: '导入'
+        },
+        {
+          icon: '',
+          text: '导出'
+        },
+      ]
     }
   },
   computed: {
@@ -38,6 +100,11 @@ export default {
     },
     contentHeight() {
       return (document.body.clientHeight || document.documentElement.clientHeight || 0) - 60;
+    }
+  },
+  methods: {
+    showBackground(index) {
+      
     }
   }
 }
--- src/views/addData/addArticle/index.vue
+++ src/views/addData/addArticle/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="user-list" :style="{ height: `${contentHeight}px` }">
-    <div class="edit-tool"></div>
+    <div class="edit-tool">
+      <div class="author">
+        <!-- <i class="el-icon-arrow-left author-back"></i> -->
+        <span>创作中心</span>
+      </div>
+      <div class="md-tool">
+        <i class="el-icon-refresh tool-icon" v-for="(item, index) in toolArr" :key="index" @mousemove="showBackground(index)"></i>
+      </div>
+    </div>
     <div class="edit" :style="{ height: `${contentHeight - 74}px` }">
       <div class="entry-mes">
         <textarea v-model="articleContent" class="art-edit"></textarea>
@@ -32,7 +40,8 @@ export default Controller;
 .show-template {
   flex: 1;
   height: 100%;
-  padding: 20px;overflow: hidden;
+  padding: 20px;
+  overflow: hidden;
 }
 
 .show-template {
@@ -48,6 +57,28 @@ export default Controller;
   height: 44px;
   line-height: 44px;
   background: #e0e0e0;
+  display: flex;
+  align-items: center;
+  color: #666;
+
+  .author {
+    font-size: 20px;
+    font-weight: bold;
+    padding: 0 10px;
+  }
+
+  .md-tool {
+    padding: 0 20px 0 50px;
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    .tool-icon {
+      font-size: 18px;
+      font-weight: bold;
+    }
+  }
 }
 
 .art-edit {
